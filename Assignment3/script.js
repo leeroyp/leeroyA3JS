f179462974bca2e9a068e1846ce373e4054f4163
Leeroy Phili <leeroyphili@gmail.com>
2020-02-10 22:38:08 -0500

    generator functions added to the script

--- Assignment3/script.js
+++ Assignment3/script.js
@@ -1,14 +1,69 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector(".btn");
+ 
+function criteria(){
+  var plength 
+  var characters  
+  
+
+  plength = prompt("Please select the length of password between 8 and 128");
+  // characters = prompt("please select the type characters you would like to use");
+  
+  while (plength < 8 || plength > 128) {
+    plength = prompt("Length must be 8-128 characters. How many characters would you like your password to be?");
+  }
+  characters=prompt("please input all types of characters you would like to use ")
+
+  // while( characters!= lowercase, uppercase, numbers , special characters){
+
+  // }
+}
+generateBtn.addEventListener('click', criteria);
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  // var password = generatePassword();
   var passwordText = document.querySelector("#password");
+  var char = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ "
+  var num ='0123456789'
+  var sym ="!@#$%^&*()_+~`|}{[]\:;?><,./-="
 
-  passwordText.value = password;
+
+ 
+  function password (){
+    var pwd ="";
+  
+    for( i=0; i<lenght; i++){
+      pwd+= characters.charAt(Math.floor(Math.random()+characters.length))
+    }
+  
+    return pwd
+  }
+ 
+ 
+ 
+  // passwordText.value = password;
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
+
+
+
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
+
+
