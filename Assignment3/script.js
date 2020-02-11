e381d6394660ce8d706f3ffc20b987973074e035
Leeroy Phili <leeroyphili@gmail.com>
2020-02-10 23:21:27 -0500

    prompts elements added

--- Assignment3/script.js
+++ Assignment3/script.js
@@ -1,69 +1,88 @@
-// Assignment Code
-var generateBtn = document.querySelector(".btn");
- 
-function criteria(){
-  var plength 
-  var characters  
-  
+// DOM elements
 
-  plength = prompt("Please select the length of password between 8 and 128");
-  // characters = prompt("please select the type characters you would like to use");
-  
-  while (plength < 8 || plength > 128) {
-    plength = prompt("Length must be 8-128 characters. How many characters would you like your password to be?");
-  }
-  characters=prompt("please input all types of characters you would like to use ")
-
-  // while( characters!= lowercase, uppercase, numbers , special characters){
-
-  // }
-}
-generateBtn.addEventListener('click', criteria);
+const generateEl = document.getElementById('generate')
 
 
+// const randomFunc = {
+//     lower: getRandomLower,
+//     upper: getRandomUpper,
+//     number: getRandomNumber, 
+//     symbol: getRandomSymbol
+// };
 
-
-
-
-
-
-
-
-
-
-
+generateEl.addEventListener('click',writePassword)
 
 // Write password to the #password input
 function writePassword() {
-  // var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  var char = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ "
-  var num ='0123456789'
-  var sym ="!@#$%^&*()_+~`|}{[]\:;?><,./-="
-
-
- 
-  function password (){
-    var pwd ="";
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
   
-    for( i=0; i<lenght; i++){
-      pwd+= characters.charAt(Math.floor(Math.random()+characters.length))
-    }
+    passwordText.value = password;
   
-    return pwd
   }
- 
- 
- 
-  // passwordText.value = password;
+
+
+
+// Prompts that come up after you click generate password
+function generatePassword() {
+    var passwordLength = prompt("Please enter the number of characters you want for you new password.  It must be more than 12 but less than 128.");
+  
+    var numbers = confirm("Do you want numbers in your password?");
+  
+    var lowerCases = confirm("Do you want lowercases in your password?");
+  
+    var upperCases = confirm("Do you want uppercases in your password?");
+  
+    var special = confirm("Do you want special characters in your password?");
+  
+    // this is a minimum count for numbers, lowerCases, upperCases & specialCharacters
+    var minimumCount = 0;
+
+
+// Generator functions
+
+
+function getRandomLower() {
+    return String.fromCharCode(Math.floor(Math.random() * 26)+ 97);
+}
+
+function getRandomUpper() {
+    return String.fromCharCode(Math.floor(Math.random() * 26)+ 65);
+}
+
+function getRandomNumber() {
+    return String.fromCharCode(Math.floor(Math.random() * 10)+ 48);
+}
+
+function getRandomSymbol() {
+    const symboles = '!@#$%^&*(){}[]=<>/,.'
+    return symbols[Math.floor(Math.random() * symbols.length)];
+}
+};
+
+
+// Checks to make sure user selected ok for all and uses empty minimums from above
+
+if (numbers === true) {
+  minimumNumbers = functionArray.getRandomNumber();
+  minimumCount++;
 
 }
 
+if (lowerCases === true) {
+  minimumLowerCases = functionArray.getRandomLower();
+  minimumCount++;
 
+}
 
+if (upperCases === true) {
+  minimumUpperCases = functionArray.getRandomUpper();
+  minimumCount++;
 
+}
 
-// // Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
+if (special === true) {
+  minimumSpecialCharacters = functionArray.getRandomSymbols();
+  minimumCount++;
 
-
+}
